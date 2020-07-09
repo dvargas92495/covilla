38940a08c865606977a9c4997571d81bc0dacefe
David Vargas <dvargas92495@gmail.com>
2020-07-08 20:02:53 -0400

    Tooltips

--- components/ExpandedMarker.js
+++ components/ExpandedMarker.js
@@ -1,26 +1,38 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import React from "react";
-import styles from '../util/Styles.module.css';
+import styles from "../util/Styles.module.css";
 
 class ExpandedMarker extends React.Component {
-  render () {
-    const marker = this.props.marker;
+  render() {
+    const marker = this.props.marker || { people: [] };
     return (
-      <center>
+      <center style={{ padding: 16 }}>
         <div>{marker.dates}</div>
         <div>{marker.location}</div>
         <div className={styles.dotList}>
-          {marker.people.map(p => {
-            return p.url.length ? <a className={styles.link} href={p.url} target="_blank" rel="noopener noreferrer">{p.name}</a> : <span>{p.name}</span>
+          {marker.people.map((p, i) => {
+            return p.url.length ? (
+              <a
+                className={styles.link}
+                href={p.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={i}
+              >
+                {p.name}
+              </a>
+            ) : (
+              <span key={i}>{p.name}</span>
+            );
           })}
         </div>
       </center>
     );
   }
-};
+}
 
 ExpandedMarker.propTypes = {
-  marker: PropTypes.object.isRequired
+  marker: PropTypes.object.isRequired,
 };
 
 export default ExpandedMarker;
--- components/MapChart.js
+++ components/MapChart.js
@@ -3,77 +3,97 @@ import {
   ComposableMap,
   Geographies,
   Geography,
-  Marker
+  Marker,
 } from "react-simple-maps";
-import markers from '../util/markers';
+import markers from "../util/markers";
 import ExpandedMarker from "./ExpandedMarker";
-import Modal from "./Modal";
+import Popover from "@material-ui/core/Popover";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
 class MapChart extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {};
-    }
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      marker: null,
+    };
+  }
 
-    onClick = marker => () => {
-        this.setState({ marker });
-    }
+  onClick = (marker) => (e) => {
+    this.setState({ marker, anchorEl: e.currentTarget });
+  };
 
-    render () {
-        const marker = this.state.marker;
-        return (
-            <>
-                <ComposableMap width={2000} projection="geoAlbersUsa">
-                <Geographies geography={geoUrl}>
-                    {({ geographies }) => (
-                        <>
-                            {geographies.map(geo => (
-                            <Geography
-                                key={geo.rsmKey}
-                                geography={geo}
-                                fill="#DDD"
-                                style={{
-                                    default: {
-                                        fill: "#D6D6DA",
-                                        outline: "none"
-                                    },
-                                    hover: {
-                                        fill: "#F53",
-                                        outline: "none"
-                                    },
-                                    pressed: {
-                                        outline: "none"
-                                    }
-                                }}
-                            />
-                            ))}
-                        </>
-                    )}
-                </Geographies>
-                {Object.values(markers).map(({ location, coordinates }) => (
-                    <Marker key={location} coordinates={coordinates}>
-                    <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} onClick={this.onClick(location)} />
-                    <text
-                        textAnchor="middle"
-                        y={25}
-                        style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-                    >
-                        {location}
-                    </text>
-                    </Marker>
+  handleClose = () => this.setState({ marker: null, anchorEl: null });
+
+  render() {
+    const marker = this.state.marker;
+    const anchorEl = this.state.anchorEl;
+    return (
+      <>
+        <ComposableMap width={2000} projection="geoAlbersUsa">
+          <Geographies geography={geoUrl}>
+            {({ geographies }) => (
+              <>
+                {geographies.map((geo) => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="#DDD"
+                    style={{
+                      default: {
+                        fill: "#D6D6DA",
+                        outline: "none",
+                      },
+                      hover: {
+                        fill: "#F53",
+                        outline: "none",
+                      },
+                      pressed: {
+                        outline: "none",
+                      },
+                    }}
+                  />
                 ))}
-                </ComposableMap>
-                {marker && (
-                    <Modal 
-                        onClose={this.onClick()}
-                        content={<ExpandedMarker marker={markers[marker]} />}
-                    />
-                )}
-            </>
-        );
-    }
-};
+              </>
+            )}
+          </Geographies>
+          {Object.values(markers).map(({ location, coordinates }) => (
+            <Marker key={location} coordinates={coordinates}>
+              <circle
+                r={10}
+                fill="#F00"
+                stroke="#fff"
+                strokeWidth={2}
+                onClick={this.onClick(location)}
+              />
+              <text
+                textAnchor="middle"
+                y={25}
+                style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+              >
+                {location}
+              </text>
+            </Marker>
+          ))}
+        </ComposableMap>
+        <Popover
+          open={!!marker}
+          anchorEl={anchorEl}
+          onClose={this.handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <ExpandedMarker marker={markers[marker]} />
+        </Popover>
+      </>
+    );
+  }
+}
 
 export default MapChart;
