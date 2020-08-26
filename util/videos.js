ad62a502a791c16b568b49905f87a1dbdea99863
Nikhil Punwaney <nikhilpunwaney@gmail.com>
2020-08-26 16:03:37 -0400

    More vids

--- util/videos.js
+++ util/videos.js
@@ -3,5 +3,30 @@ export default [
         title: 'Boston MA to Davenport FL (Part 1) 7-31-2020',
         url: 'https://www.youtube.com/embed/kOpQotMhLc0',
         date: '7/31/2020'
+    },
+    {
+        title: 'Boston MA to Davenport FL (Part 2) 7-31-2020',
+        url: 'https://www.youtube.com/embed/1e3Mnespt1c',
+        date: '7/31/2020'
+    },
+    {
+        title: 'Boston MA to Davenport FL (Part 3) 7-31-2020',
+        url: 'https://www.youtube.com/embed/qDaDXIyhWwI',
+        date: '7/31/2020'
+    },
+    {
+        title: 'Boston MA to Davenport FL (Part 4) 7-31-2020',
+        url: 'https://www.youtube.com/embed/mIy66s4g3vk',
+        date: '7/31/2020'
+    },
+    {
+        title: 'Boston MA to Davenport FL (Part 5) 7-31-2020',
+        url: 'https://www.youtube.com/embed/or5ynDN7_yk',
+        date: '7/31/2020'
+    },
+    {
+        title: 'Boston MA to Davenport FL (Part 6) 7-31-2020',
+        url: 'https://www.youtube.com/embed/VI7RoW9OZK4',
+        date: '7/31/2020'
     }
 ];
